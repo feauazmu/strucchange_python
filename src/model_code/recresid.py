a22e1b91586bf1417da625ff6a6aaf7462a3116d
Felipe A. Azuero M <azuero@protonmail.com>
2020-03-05 04:41:04 +0100

    Fixed recresid function in recresid.py file.

--- src/model_code/recresid.py
+++ src/model_code/recresid.py
@@ -1,23 +1,24 @@
 import numpy as np
 
+
 def recresid(x, y):
     n = x.shape[0]
     q = x.shape[1]
     rval = np.zeros(n - q)
     y1 = y[0:q]
-    Q,R = np.linalg.qr(x[0:q])
-    X1 = np.linalg.inv(R.T.dot(R))
-    betar = np.nan_to_num(np.linalg.inv(R).dot(Q.T).dot(y1))
-    xr = x[q,:]
-    fr = 1 + (xr.T.dot(X1).dot(xr))
-    rval[0] = (y[q] - xr.T.dot(betar))/np.sqrt(fr)
+    Q, R = np.linalg.qr(x[0:q])
+    X1 = np.linalg.inv(np.dot(R.T, R))
+    betar = np.nan_to_num(np.dot(np.dot(np.linalg.inv(R), Q.T), y1))
+    xr = x[q, :]
+    fr = 1 + (np.dot(np.dot(xr.T, X1), xr))
+    rval[0] = (y[q] - np.dot(xr.T, betar)) / np.sqrt(fr)
 
     if q < n:
-        for r in range(q+1, n):
-            X1 = X1 - (X1.dot(np.outer(xr, xr)).dot(X1))/fr
-            betar = (betar.T + X1.dot(xr) * rval[r-q-1] * np.sqrt(fr)).T
-            xr = x[r,:]
-            fr = 1 + xr.T.dot(X1).dot(xr)
-            rval[r-q] = (y[r] - np.sum(xr * betar.T))/np.sqrt(fr)
+        for r in range(q + 1, n):
+            X1 = X1 - (np.dot(np.dot(X1, np.outer(xr, xr)), X1)) / fr
+            betar = (betar.T + np.dot(X1, xr) * rval[r - q - 1] * np.sqrt(fr)).T
+            xr = x[r, :]
+            fr = 1 + np.dot(np.dot(xr.T, X1), xr)
+            rval[r - q] = (y[r] - np.sum(xr * betar.T)) / np.sqrt(fr)
 
     return rval
