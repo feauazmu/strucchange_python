ef658c924c934e2354db11a0fac1566806bfdbff
Felipe A. Azuero M <azuero@protonmail.com>
2020-03-07 15:14:30 +0100

    Added empirical analysis part.

---
+++ src/analysis/empirical_analysis.py
@@ -0,0 +1,42 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from bld.project_paths import project_paths_join as ppj
+from src.model_code.breakpoints import breakpoints
+
+# load the data.
+col_hom_rate = pd.read_csv(ppj("OUT_DATA", "col_historic_homicide.csv"))
+
+# extract information from the data
+years = col_hom_rate["Year"].tolist()
+hom_rates = col_hom_rate["Homicide"].tolist()
+
+# find optimal breakpoints.
+breakpoints_col = breakpoints("Homicide ~ 1", col_hom_rate)
+
+opt_breaks = breakpoints_col[0]
+opt_obs = breakpoints_col[1]
+
+# find date of breaks.
+if opt_breaks == 0:
+    opt_dates = np.nan
+else:
+    opt_dates = []
+    for i in opt_obs:
+        opt_dates.append(years[i])
+
+
+# build dictionary with data.
+breakpoint_info = {
+    "Years": years,
+    "Homicide Rate": hom_rates,
+    "Optimal Number of Breaks": opt_breaks,
+    "Observation of Breaks": opt_obs,
+    "Optimal Dates": opt_dates,
+}
+
+# store the information of the breakpoint analysis.
+with open(ppj("OUT_ANALYSIS", "colombia_hom_breaks.pickle"), "wb") as out_file:
+    pickle.dump(breakpoint_info, out_file)
